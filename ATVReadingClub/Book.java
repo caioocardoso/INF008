29d201ff7e77efb5df882880b2abf9cafc311ec7
caioocardoso <caiooliveiracardosoo@icloud.com>
2025-05-13 18:13:32 -0300

    .

---
+++ ATVReadingClub/Book.java
@@ -0,0 +1,10 @@
+Class Book {
+    int id;
+    String title;
+    String author;
+    int recomendations = 0;
+
+    public incrementRecomendation(){
+        recomendations++;
+    }
+}
--- ATVReadingClub/Member.java
+++ ATVReadingClub/Member.java
@@ -1,3 +1,13 @@
 Class Member {
-    
+    private String cpf;
+    private String name;
+
+
+    public Member(String cpf, String name){
+        this.cpf = cpf;
+        this.name = name;
+    }
+
+    public recomendate(int id){
+    }
 }
