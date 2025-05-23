0a81f09ee30905f9fdc77be6d0d0addb701e70a6
caioocardoso <caiooliveiracardosoo@icloud.com>
2025-05-23 17:27:48 -0300

    better formatted

--- FirstWork/src/main/java/com/INF008/app/Menu.java
+++ FirstWork/src/main/java/com/INF008/app/Menu.java
@@ -51,15 +51,21 @@ public class Menu {
                     break;
                 case 0:
                     Utils.cleanScreen();
-                    System.out.println("╔══════════════════════════════════════════════════════════════════════════════");
-                    System.out.println("║\t\t\t" + ConsoleColors.WARNING + "Leaving the system..." + ConsoleColors.RESET);
-                    System.out.println("╠══════════════════════════════════════════════════════════════════════════════");
+                    System.out
+                            .println("╔══════════════════════════════════════════════════════════════════════════════");
+                    System.out
+                            .println("║\t\t\t" + ConsoleColors.WARNING + "Leaving the system..." + ConsoleColors.RESET);
+                    System.out
+                            .println("╠══════════════════════════════════════════════════════════════════════════════");
                     break;
                 default:
                     Utils.cleanScreen();
-                    System.out.println("╔══════════════════════════════════════════════════════════════════════════════");
-                    System.out.println("║\t\t\t" + ConsoleColors.WARNING + "Invalid option, please try again." + ConsoleColors.RESET);
-                    System.out.println("╠══════════════════════════════════════════════════════════════════════════════");
+                    System.out
+                            .println("╔══════════════════════════════════════════════════════════════════════════════");
+                    System.out.println("║\t\t\t" + ConsoleColors.WARNING + "Invalid option, please try again."
+                            + ConsoleColors.RESET);
+                    System.out
+                            .println("╠══════════════════════════════════════════════════════════════════════════════");
                     try {
                         Thread.sleep(2000);
                     } catch (InterruptedException e) {
@@ -69,44 +75,79 @@ public class Menu {
     }
 
     public static void registerEvent() {
+        int type;
+        do {
+            Utils.cleanScreen();
+            System.out.println("╔══════════════════════════════════════════════════════════════════════════════");
+            System.out.println("║\t\t\t" + ConsoleColors.INFO + "REGISTER EVENT" + ConsoleColors.RESET);
+            System.out.println("╠══════════════════════════════════════════════════════════════════════════════");
+            System.out.println("║\t" + ConsoleColors.INFO + "1. Lecture." + ConsoleColors.RESET);
+            System.out.println("║\t" + ConsoleColors.INFO + "2. Workshop." + ConsoleColors.RESET);
+            System.out.println("║\t" + ConsoleColors.INFO + "3. Short course." + ConsoleColors.RESET);
+            System.out.println("║\t" + ConsoleColors.INFO + "4. Academic fair" + ConsoleColors.RESET);
+            System.out.println("║\t" + ConsoleColors.INFO + "0. Leave" + ConsoleColors.RESET);
+            System.out.println("╚══════════════════════════════════════════════════════════════════════════════");
+            System.out.print(ConsoleColors.INPUT + "Choose an option: " + ConsoleColors.RESET);
+
+            type = scanner.nextInt();
+            scanner.nextLine();
+
+            if (type >= 1 && type <= 4) {
+                System.out.print(ConsoleColors.INPUT + "Title: " + ConsoleColors.RESET);
+                String title = scanner.nextLine();
+
+                System.out.print(ConsoleColors.INPUT + "Date: " + ConsoleColors.RESET);
+                String date = scanner.nextLine();
+
+                System.out.print(ConsoleColors.INPUT + "Local: " + ConsoleColors.RESET);
+                String local = scanner.nextLine();
+
+                System.out.print(ConsoleColors.INPUT + "Capacity: " + ConsoleColors.RESET);
+                int capacity = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print(ConsoleColors.INPUT + "Description: " + ConsoleColors.RESET);
+                String description = scanner.nextLine();
+            }
+
+            switch (type) {
+                case 1:
+                    eventRegisteredSuccessfull();
+                    break;
+                case 2:
+                    eventRegisteredSuccessfull();
+                    break;
+                case 3:
+                    eventRegisteredSuccessfull();
+                    break;
+                case 4:
+                    eventRegisteredSuccessfull();
+                    break;
+                case 0:
+                    return;
+                default:
+                    Utils.cleanScreen();
+                    System.out
+                            .println("╔══════════════════════════════════════════════════════════════════════════════");
+                    System.out.println("║\t\t\t" + ConsoleColors.WARNING + "Invalid option, please try again."
+                            + ConsoleColors.RESET);
+                    System.out
+                            .println("╠══════════════════════════════════════════════════════════════════════════════");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                    }
+                    break;
+            }
+        } while (type != 0);
+    }
+
+    public static void eventRegisteredSuccessfull() {
         Utils.cleanScreen();
         System.out.println("╔══════════════════════════════════════════════════════════════════════════════");
-        System.out.println("║\t\t\t" + ConsoleColors.INFO + "REGISTER EVENT" + ConsoleColors.RESET);
+        System.out.println("║\t\t\t" + ConsoleColors.INFO + "EVENT REGISTER SUCCESSFULL" + ConsoleColors.RESET);
         System.out.println("╠══════════════════════════════════════════════════════════════════════════════");
-        System.out.println("║\t" + ConsoleColors.INFO + "1. Lecture." + ConsoleColors.RESET);
-        System.out.println("║\t" + ConsoleColors.INFO + "2. Workshop." + ConsoleColors.RESET);
-        System.out.println("║\t" + ConsoleColors.INFO + "3. Short course." + ConsoleColors.RESET);
-        System.out.println("║\t" + ConsoleColors.INFO + "4. Academic fair" + ConsoleColors.RESET);
-        System.out.println("║\t" + ConsoleColors.INFO + "0. Leave" + ConsoleColors.RESET);
-        System.out.println("╚══════════════════════════════════════════════════════════════════════════════");
-        System.out.print(ConsoleColors.INPUT + "Choose an option: " + ConsoleColors.RESET);
 
-        int type;
-        type = scanner.nextInt();
-        scanner.nextLine();
-        if (type == 0)
-            return;
-
-        System.out.print("Title: ");
-        String title = scanner.nextLine();
-
-        System.out.print("Date: ");
-        String date = scanner.nextLine();
-
-        System.out.print("Local: ");
-        String local = scanner.nextLine();
-
-        System.out.print("Capacity: ");
-        int capacity = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Description: ");
-        String description = scanner.nextLine();
-
-        Utils.cleanScreen();
-        System.out.println("-------------------------------------------------------------------------------");
-        System.out.println("\t\t\tEVENT REGISTER SUCCESSFULL");
-        System.out.println("-------------------------------------------------------------------------------");
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
